feaa354d147f9dc52fa93d0ee6fe4291af41ce0e
Oleg Ilin <ilyinon@mail.ru>
2026-02-02 18:53:09 +0300

    iter6

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/ilyinon/go-musthave-metrics/internal/handler"
+	appmw "github.com/ilyinon/go-musthave-metrics/internal/middleware"
 	"github.com/ilyinon/go-musthave-metrics/internal/repository"
 
 	"github.com/go-chi/chi/v5"
@@ -14,7 +15,7 @@ func New(storage repository.Storage) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	r.Use(appmw.Logger)
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", handler.NewIndex(storage).ServeHTTP)
