e9e72b62896f27ca9d40e310dde1705880de68fd
Oleg Ilin <ilyinon@mail.ru>
2026-02-02 16:16:56 +0300

    добавить энвы для сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ilyinon/go-musthave-metrics/internal/config"
 	"github.com/ilyinon/go-musthave-metrics/internal/repository/mem"
@@ -11,7 +12,18 @@ import (
 )
 
 func main() {
-	addr := &config.ServerAddress{}
+	// ===== default =====
+	addr := &config.ServerAddress{
+		Host: "localhost",
+		Port: 8080,
+	}
+
+	// ===== env override =====
+	if v, ok := os.LookupEnv("ADDRESS"); ok {
+		_ = addr.Set(v)
+	}
+
+	// ===== flag override env =====
 	flag.Var(addr, "a", "server address")
 	flag.Parse()
 
