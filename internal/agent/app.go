89c0e8fd061bb924ffe5a884364248d18f29f64b
Oleg <ilyinon@mail.ru>
2026-03-07 15:56:40 +0200

    Iter15 (#15)
    * iter15

--- internal/agent/app.go
+++ internal/agent/app.go
@@ -2,7 +2,7 @@ package agent
 
 import (
 	"log"
-	"sync/atomic"
+	"sync"
 	"time"
 
 	"github.com/ilyinon/go-musthave-metrics/internal/agent/collector"
@@ -10,26 +10,36 @@ import (
 	"github.com/ilyinon/go-musthave-metrics/internal/model"
 )
 
+const defaultRateLimit = 1
+
 type App struct {
 	client         *sender.Client
 	serverURL      string
 	pollInterval   time.Duration
 	reportInterval time.Duration
+	rateLimit      int
 
-	gauges   atomic.Value
-	counters atomic.Value
+	mu       sync.RWMutex
+	gauges   model.RuntimeMetrics
+	counters model.CustomMetrics
 }
 
 func New(
 	client *sender.Client,
 	serverURL string,
 	poll, report time.Duration,
+	rateLimit int,
 ) *App {
+	if rateLimit <= 0 {
+		rateLimit = defaultRateLimit
+	}
+
 	return &App{
 		client:         client,
 		serverURL:      serverURL,
 		pollInterval:   poll,
 		reportInterval: report,
+		rateLimit:      rateLimit,
 	}
 }
 
@@ -39,53 +49,74 @@ func (a *App) Run() {
 	defer pollTicker.Stop()
 	defer reportTicker.Stop()
 
+	sendCh := make(chan []model.Metrics, a.rateLimit)
+
+	for i := 0; i < a.rateLimit; i++ {
+		go func() {
+			for batch := range sendCh {
+				if err := a.client.Batch(batch); err != nil {
+					log.Printf("ERROR: batch send failed, fallback to single: %v", err)
+
+					for _, m := range batch {
+						switch m.MType {
+						case model.MetricGauge:
+							a.client.Gauge(m.ID, *m.Value)
+						case model.MetricCounter:
+							a.client.Counter(m.ID, *m.Delta)
+						}
+					}
+				}
+			}
+		}()
+	}
+
 	for {
 		select {
 		case <-pollTicker.C:
-			a.gauges.Store(collector.Runtime())
-			a.counters.Store(collector.Custom())
+			g := collector.Runtime()
+
+			for k, v := range collector.Gopsutil() {
+				g[k] = v
+			}
+
+			c := collector.Custom()
+
+			a.mu.Lock()
+			a.gauges = g
+			a.counters = c
+			a.mu.Unlock()
 
 		case <-reportTicker.C:
 			var batch []model.Metrics
 
-			if g, ok := a.gauges.Load().(model.RuntimeMetrics); ok {
-				for k, v := range g {
-					val := v
-					batch = append(batch, model.Metrics{
-						ID:    k,
-						MType: model.MetricGauge,
-						Value: &val,
-					})
-				}
+			a.mu.RLock()
+			g := a.gauges
+			c := a.counters
+			a.mu.RUnlock()
+
+			for k, v := range g {
+				val := v
+				batch = append(batch, model.Metrics{
+					ID:    k,
+					MType: model.MetricGauge,
+					Value: &val,
+				})
 			}
 
-			if c, ok := a.counters.Load().(model.CustomMetrics); ok {
-				for k, v := range c {
-					delta := v
-					batch = append(batch, model.Metrics{
-						ID:    k,
-						MType: model.MetricCounter,
-						Delta: &delta,
-					})
-				}
+			for k, v := range c {
+				delta := v
+				batch = append(batch, model.Metrics{
+					ID:    k,
+					MType: model.MetricCounter,
+					Delta: &delta,
+				})
 			}
 
 			if len(batch) == 0 {
 				continue
 			}
 
-			if err := a.client.Batch(batch); err != nil {
-				log.Printf("batch send failed, fallback to single: %v", err)
-
-				for _, m := range batch {
-					switch m.MType {
-					case model.MetricGauge:
-						_ = a.client.Gauge(m.ID, *m.Value)
-					case model.MetricCounter:
-						_ = a.client.Counter(m.ID, *m.Delta)
-					}
-				}
-			}
+			sendCh <- batch
 		}
 	}
 }
